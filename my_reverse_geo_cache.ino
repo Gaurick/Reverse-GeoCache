b1ea0dfc68c515ad92d5848a3e6166112bfe2b02
Erik Oasen <Erik.I.Oasen@gmail.com>
2018-10-23 13:46:17 -0400

    Update my_reverse_geo_cache.ino
    changed a bunch of stuff.
    see change note in beginning of program for specifics.

--- my_reverse_geo_cache.ino
+++ my_reverse_geo_cache.ino
@@ -33,7 +33,6 @@
  *  A to +5v.
  *  K to gnd
  *  
- *  
  *  LCD pot
  *  one side +5v.
  *  other side gnd.
@@ -43,7 +42,18 @@
  *  distance from the destination is measured in meters.
  *  changes rEarth to the radius of earth in whatever measurement you'd prefer.
  *  
- *  first finished 9/19/18.
+ *  v 1.0 first finished 9/19/18.
+ *  
+ *  v 1.1 finished 10/23/18.
+ *  separated the distance calculations for longitude and latitude to it's own function.
+ *  moved everything but the timer to a separate function and dumped into a switch case statement to step through.
+ *  put in a timer (about 15 seconds) of unlocking when you first turn it on to stuff things in, test, or just fix.
+ *  put in the ability to have waypoints on the way to the final location to unlock (currently it's 2 waypoints and the final location).
+ *  put a servo wiggle when the box gets to a waypoint to let you know you've gotten close enough.
+ *  removed the decimal part of the distance and added "m" to show it's in meters and not feet or whatever.
+ *  set update for lcd and all steps to every second instead of every 2 seconds.
+ *  more comments.
+ *  probably more that i can't remember.
  * 
  */
 
@@ -76,9 +86,15 @@ PWMServo myservo;
 int pos = 0;
 //variable to store the servo position.
 
-float Lat2 = 33.761685 * 0.0174533;
-float Long2 = -82.278742 * 0.0174533;
-//gps location of the destination to unlock the box.
+float Lat1 = 33.761685 * 0.0174533;
+float Long1 = -82.278742 * 0.0174533;
+//gps location of the first point needed to unlock the box.
+float Lat2 = 33.761450 * 0.0174533;
+float Long2 = -82.278070 * 0.0174533;
+//gps location of the second point needed to unlock the box.
+float Lat3 = 33.761685 * 0.0174533;
+float Long3 = -82.278742 * 0.0174533;
+//gps location of the first point needed to unlock the box.
 //needs to be in decimal degrees and not in the standard way.
 //then convert the degrees to radians.
 const float rEarth = 6371000.0;
@@ -86,7 +102,16 @@ const float rEarth = 6371000.0;
 //in meters
 bool lock = 0;
 //variable to track if it's been closer than 20 meters to the location.
-float d = 0;
+int d = 0;
+//variable for the distance to the destination.
+
+int steps = 0;
+//variable to track what's been done.
+int counter = 0;
+//counter variable to be used to count.
+
+float h = 0;
+
 
 void setup() {
   lcd.begin(16, 2);
@@ -159,56 +184,147 @@ void loop() {
   if (timer > millis())  timer = millis();
   //reset the timer if it's time to reset.
   
-  if (millis() - timer > 2000) { 
+  if (millis() - timer > 1000) { 
     //about every 2 seconds print the update.
-    timer = millis(); // reset the timer
+    timer = millis(); 
+    // reset the timer
+    if(counter == 16){
+      counter = 0;
+    }
 
+    else{
+      counter ++;
+    }
+    
+    stages();
+  }
+}
+
+
+void stages(){
+  switch (steps){
+    case 0:
+    //stage to allow person time to put things into the box before it locks.
+    if(counter < 15){
+      //if the count down is still counting, show it.
+      lcd.clear();
+      lcd.print("Locking in");
+      lcd.setCursor(0, 1);
+      lcd.print(15 - counter);
+      myservo.write(180);
+    }
+
+    else{
+      //if the count down is done counting lock the box and move to the next step.
+      myservo.write(0);
+      steps ++;
+    }
+    break;
+
+    case 1:
     if(GPS.fix){
-      //if the gps is connected do things.
-      float Lat1 = ((GPS.latitudeDegrees) * 0.0174533);
-      float Long1 = ((GPS.longitudeDegrees) * 0.0174533);
-      
-      float h = sq((sin((Lat1 - Lat2) / 2.0))) + (cos(Lat1) * cos(Lat2) * sq((sin((Long1 - Long2) / 2.0))));
-      d = 2.0 * rEarth * asin (sqrt(h)); 
-      //maths to calculate the distance between two points using longitude and latitude.
-      //shamelessly borrowed from,
-      //https://learn.adafruit.com/reverse-geocache-engagement-box
-      
-      if(d < 20){
-        //if you're within 20 meters of the point.
-        lock = 1;
-        //set the lock flag to 1.
-      }
-      showLock();
+      //if the gps unit has a fix on it's current location, skip this step.
+      steps ++;
     }
 
     else{
       //if the gps isn't connected, say so.
-      lcd.setCursor(0, 0);
+      lcd.clear();
       lcd.print("Acquiring");
       lcd.setCursor(0, 1);
       lcd.print("signal");
     }
+    break;
+    
+
+    case 2:
+    //run the first location into the distance calculation function to figure out how far you're from the first point.
+    distance(Lat1, Long1);
+    
+    if(d < 20){
+      //if you're within 20 meters, move on to the unlock step.
+      steps ++;
+      //wiggle the servo to make a noise that you've moved to the next step.
+      myservo.write(10);
+      delay(250);
+      myservo.write(0);
+    }
+
+    else{ 
+      //further than 20 meters.
+      lcd.clear();
+      lcd.print("Distance to 1=");
+      lcd.setCursor(0, 1);
+      lcd.print(d);
+      lcd.print(" m");
+      //tell the person how far to go.   
+    }
+    break;
+
+    case 3:
+    //feed the second location into the distance function to see how far you are from it.
+    distance(Lat2, Long2);
+      
+    if(d < 20){
+      //if you're within 20 meters, move on to the unlock step.
+      steps ++;
+      //wiggle the servo to make a noise that you've moved to the next step.
+      myservo.write(10);
+      delay(250);
+      myservo.write(0);
+    }
+
+    else{ 
+      //further than 20 meters.
+      lcd.clear();
+      lcd.print("Distance to 2=");
+      lcd.setCursor(0, 1);
+      lcd.print(d);
+      lcd.print(" m");
+      //tell the person how far to go.   
+    }
+    break;
+
+    case 4:
+    //feed the third location to the distance function to figure out how far it is from you.
+    distance(Lat3, Long3);
+      
+    if(d < 20){
+      //if you're within 20 meters, move on to the unlock step.
+      steps ++;
+    }
+
+    else{ 
+      //further than 20 meters.
+      lcd.clear();
+      lcd.print("Distance to 3=");
+      lcd.setCursor(0, 1);
+      lcd.print(d);
+      lcd.print(" m");
+      //tell the person how far to go.   
+    }
+    break;
+
+    case 5:
+    //if the final point is close enough to open, open the box.
+    lcd.clear();
+    lcd.print("The box is");
+    lcd.setCursor(0, 1); 
+    lcd.print("now unlocked.");
+    myservo.write(180);
+    //move the servo to open the box.
+    break;   
   }
 }
 
-void showLock(){
-  if(lock == 1){
-    //if the lock flag has been set to 1 then unlock.
-     lcd.setCursor(0, 0);
-     lcd.print("The box is");
-     lcd.setCursor(0, 1); 
-     lcd.print("now unlocked.");
-     myservo.write(180);
-     //move the servo to open the box.
-  }
-
-  else{
-    //further than 20 meters.
-    lcd.setCursor(0, 0);
-    lcd.print("Distance=");
-    lcd.setCursor(0, 1);
-    lcd.print(d);
-    //tell the person how far to go.    
-  }
+void distance(float lat2, float long2){
+  //calculate the distance from your current location to the location you need to go to to unlock the step.
+    float lat1 = ((GPS.latitudeDegrees) * 0.0174533);
+    float long1 = ((GPS.longitudeDegrees) * 0.0174533);
+      
+    h = sq((sin((lat1 - lat2) / 2.0))) + (cos(lat1) * cos(lat2) * sq((sin((long1 - long2) / 2.0))));
+    d = 2.0 * rEarth * asin (sqrt(h)); 
+    //maths to calculate the distance between two points using longitude and latitude.
+    //shamelessly borrowed from,
+    //https://learn.adafruit.com/reverse-geocache-engagement-box
 }
